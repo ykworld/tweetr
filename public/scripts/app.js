ca88a685a56b3cc00c406889cab5ca5996111b14
ykworld <yonseung.kim@gmail.com>
2017-06-13 20:00:13 +0000

    creating the tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,18 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+
+  $(".tweet").hover(
+    function() {
+      $(this).addClass("tweet_hover");
+      $(this).children("header").addClass("tweet_header_hover");
+      $(this).find(".icons").addClass("tweet_footer_hover");
+    }, function() {
+      $(this).removeClass("tweet_hover");
+      $(this).children("header").removeClass("tweet_header_hover");
+      $(this).find(".icons").removeClass("tweet_footer_hover");
+    }
+  );
+
+});
